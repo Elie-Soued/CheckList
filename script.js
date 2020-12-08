680f1f5cbe400d93b772dc143ed400c2969b29dc
Elie <eliegebransoued@gmail.com>
2020-12-08 18:08:23 +0100

    JS trying to create an Eventlistener

--- script.js
+++ script.js
@@ -11,16 +11,38 @@
 // 5- The div has to be appended to a checklistbody     (callback function)
 
 
-function addNameToList(name) {
-  // Selects all html elements with a class of names
-  const namesDiv = document.querySelector(".names");
-  // Create an html element
-  const nameNode = document.createElement("div");
-  // Inserting in the created div some html / text
-  nameNode.innerHTML = `${name} was here`;
-  namesDiv.appendChild(nameNode);
-}
+// function addNameToList(name) {
+//   // Selects all html elements with a class of names
+//   const namesDiv = document.querySelector(".names");
+//   // Create an html element
+//   const nameNode = document.createElement("div");
+//   // Inserting in the created div some html / text
+//   nameNode.innerHTML = `${name} was here`;
+//   namesDiv.appendChild(nameNode);
+// }
+//
+// addNameToList("Bob");
+// addNameToList("John");
+// addNameToList("Sarah");
+//
 
-addNameToList("Bob");
-addNameToList("John");
-addNameToList("Sarah");
+function addToDo() {
+  let input = String(input);}
+
+
+const checklist = document.getElementById("checklist");
+checklist.addEventListener("click", function(){
+  alert("Hi Diego")
+});
+
+
+const button = document.getElementById("button");
+console.log(button);
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+  var x = document.getElementById("checklist").value;
+  console.log(x);
+  x = document.getElementById("demo").innerHTML;
+  console.log(x);
+}
