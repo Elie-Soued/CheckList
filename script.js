9102f9e0580acb560acc58e1e09389348a47f29b
Elie <eliegebransoued@gmail.com>
2020-12-08 17:10:51 +0100

    adding info to the js file

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // We need to do 3 Things today
 
 // 1-We need to store the value of the input in a variable.
-//2-We need to listen to click of the add button
-// 3- We need to translate this variable into a string
-//4- This string has to be appended to a div
-// 5- The div has to be appended to a checklistbody
+
+
+// Eventeventlistener = event, callbackfunction
+
+//2-We need to listen to click of the add button (event)
+// 3- We need to translate this variable into a string (callback function)
+//4- This string has to be appended to a div            (callback function)
+// 5- The div has to be appended to a checklistbody     (callback function)
+
+
+function addNameToList(name) {
+  // Selects all html elements with a class of names
+  const namesDiv = document.querySelector(".names");
+  // Create an html element
+  const nameNode = document.createElement("div");
+  // Inserting in the created div some html / text
+  nameNode.innerHTML = `${name} was here`;
+  namesDiv.appendChild(nameNode);
+}
+
+addNameToList("Bob");
+addNameToList("John");
+addNameToList("Sarah");
