f502ee4308cc6996db5d19aa59470a61b11c4343
Elie <eliegebransoued@gmail.com>
2020-12-08 17:00:13 +0100

    JS steps to do

---
+++ script.js
@@ -0,0 +1,7 @@
+// We need to do 3 Things today
+
+// 1-We need to store the value of the input in a variable.
+//2-We need to listen to click of the add button
+// 3- We need to translate this variable into a string
+//4- This string has to be appended to a div
+// 5- The div has to be appended to a checklistbody
