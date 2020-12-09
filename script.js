69948e25e60d7b5a79b6d13ba73176891e1eb0a8
Elie <eliegebransoued@gmail.com>
2020-12-09 11:53:19 +0100

    output being printed using a normal button

--- script.js
+++ script.js
@@ -1,48 +1,21 @@
-// We need to do 3 Things today
-
-// 1-We need to store the value of the input in a variable.
-
-
-// Eventeventlistener = event, callbackfunction
-
-//2-We need to listen to click of the add button (event)
-// 3- We need to translate this variable into a string (callback function)
-//4- This string has to be appended to a div            (callback function)
-// 5- The div has to be appended to a checklistbody     (callback function)
-
-
-// function addNameToList(name) {
-//   // Selects all html elements with a class of names
-//   const namesDiv = document.querySelector(".names");
-//   // Create an html element
-//   const nameNode = document.createElement("div");
-//   // Inserting in the created div some html / text
-//   nameNode.innerHTML = `${name} was here`;
-//   namesDiv.appendChild(nameNode);
-// }
-//
-// addNameToList("Bob");
-// addNameToList("John");
-// addNameToList("Sarah");
-//
-
-function addToDo() {
-  let input = String(input);}
-
-
-const checklist = document.getElementById("checklist");
-checklist.addEventListener("click", function(){
-  alert("Hi Diego")
-});
-
-
+//Selection the HTLM Elements
 const button = document.getElementById("button");
-console.log(button);
-button.addEventListener("click", myFunction);
 
-function myFunction() {
-  var x = document.getElementById("checklist").value;
-  console.log(x);
-  x = document.getElementById("demo").innerHTML;
-  console.log(x);
+// Defining the call back function
+function newElement() {
+  //creating a li element
+  let li = document.createElement("li");
+  //storing the value of my input in the variable inputValue
+  let inputValue = document.getElementById("myInput").value;
+  //creating a textNode with the inputValue  and storing it in a t
+  let t = document.createTextNode(inputValue);
+  //appending t to the li
+  li.appendChild(t);
+  //selecting the checklist-wrapper
+  const todoWrapper = document.getElementById("checklistbody");
+  //appending li to the wrapper
+  todoWrapper.appendChild(li)
 }
+
+//Defining the EventListener
+button.addEventListener("click",newElement);
