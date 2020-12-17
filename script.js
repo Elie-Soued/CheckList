ead79c62bd5d519fe18f7b3a01497ddf6c342cc7
diegog999 <42815611+diegog999@users.noreply.github.com>
2020-12-17 17:54:08 +0100

    fixed it with the help of a god

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 class ToDo {
   constructor(content) {
-    this._content = content;
     this._done = false;
-    // this._tag = tag;
-  }
+    this._content = document.createElement("div");
+    this._content.classList.add("todoitem");
+    let inputValue = document.getElementById("myInput").innerText;
+    let t = document.createTextNode(inputValue);
+    let p = document.createElement("p");
+    p.classList.add("stricked");
+    p.classList.toggle("stricked");
+    p.appendChild(t);
+    this._content.appendChild(p);
 
-  markAsDone() {
-    this._done = true;
+    let edit = document.createElement("div");
+    edit.classList.add("edit");
+    edit.classList.add("clicked");
+    edit.classList.toggle("clicked");
+    this._content.appendChild(edit);
+    edit.addEventListener("click", (e) => {
+      const checkbox = e.target;
+      p.classList.toggle("stricked");
+      edit.classList.toggle("clicked");
+      this._done = true;
+    });
+
+    let remove = document.createElement("div");
+    remove.classList.add("remove");
+    this._content.appendChild(remove);
+    remove.addEventListener("click", () => {
+      todoWrapper.removeChild(this._content);
+    });
   }
 }
-
 class ToDoList {
-  constructor(toDoList) {
+  constructor() {
     this._allToDos = [];
     this._doneCounter = 0;
     this._toDoCounter = this._allToDos.length;
   }
 
   add(todo) {
-    this._allToDos.push(todo);
     this._toDoCounter++;
 
-    let div = document.createElement("div");
-    div.classList.add("todoitem");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    let p = document.createElement("p");
-    p.classList.add("stricked");
-    p.classList.toggle("stricked");
-    p.appendChild(t);
-    div.appendChild(p);
-
-    let edit = document.createElement("div");
-    edit.classList.add("edit");
-    edit.classList.add("clicked");
-    edit.classList.toggle("clicked");
-    div.appendChild(edit);
-
-    let remove = document.createElement("div");
-    remove.classList.add("remove");
-    div.appendChild(remove);
-
     const todoWrapper = document.getElementById("checklistbody");
-    todoWrapper.appendChild(div);
+    todoWrapper.appendChild(todo._content);
   }
 
   finishCurrentTask(indexOfTodo) {
@@ -50,10 +50,7 @@ class ToDoList {
     this._doneCounter++;
     this._toDoCounter--;
   }
-
-  clear() {}
 }
-// Using Methods instead of EventListeners
 
 //Selection the HTLM Elements
 const form = document.getElementById("form");
@@ -63,81 +60,11 @@ const todoWrapper = document.getElementById("checklistbody");
 //The instance from which to call the class methods
 const myChecklist = new ToDoList(todoWrapper);
 
-console.log(myChecklist);
-
-const washing = new ToDo("I am washing");
-const cleaning = new ToDo("I am cleaing");
-const myTodos = [washing, cleaning];
-const myChecklist2 = new ToDoList(myTodos);
-
-myChecklist2.add(washing);
-
-let div = document.createElement("div");
-div.classList.add("todoitem");
-let inputValue = document.getElementById("myInput").value;
-let t = document.createTextNode(inputValue);
-let p = document.createElement("p");
-p.classList.add("stricked");
-p.classList.toggle("stricked");
-p.appendChild(t);
-div.appendChild(p);
-
-console.log(div.innerText);
-
 //add event
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const task = new ToDo(div);
+  const task = new ToDo("pizza");
   myChecklist.add(task);
   console.log(task);
 });
-
-/////old ways
-// const newElement = () => {
-//   let div = document.createElement("div");
-//   div.classList.add("todoitem");
-//   let inputValue = document.getElementById("myInput").value;
-//   let t = document.createTextNode(inputValue);
-//   let p = document.createElement("p");
-//   p.classList.add("stricked");
-//   p.classList.toggle("stricked");
-//   p.appendChild(t);
-//   //todoitem
-//   div.appendChild(p);
-//   //todoList
-//   const todoWrapper = document.getElementById("checklistbody");
-//   todoWrapper.appendChild(div);
-
-//   let edit = document.createElement("div");
-//   edit.classList.add("edit");
-//   edit.classList.add("clicked");
-//   edit.classList.toggle("clicked");
-
-//   let remove = document.createElement("div");
-//   remove.classList.add("remove");
-//   div.appendChild(edit);
-//   div.appendChild(remove);
-
-//   //Finish this current Task()
-//   edit.addEventListener("click", (e) => {
-//     const checkbox = e.target;
-
-//     p.classList.toggle("stricked");
-//     edit.classList.toggle("clicked");
-//   });
-
-//   //Remove()
-//   remove.addEventListener("click", () => {
-//     todoWrapper.removeChild(div);
-//   });
-// };
-
-// // Add()
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const todo = new ToDo();
-//   todo = div;
-// });
-
-// newElement();
